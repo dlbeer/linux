2d2d7d2d217883e10f71668bb4ac6a11b83c969a
Christina Quast <contact@christina-quast.de>
2022-12-26 18:15:48 +0100

    hid-ft260: Rename struct ft260_i2c_input_report
    Rename ft260_i2c_input_report to ft260_input_report, because depending
    on the ft260 device configuration, this device can implement a UART or
    I2C functionality.
    
    Signed-off-by: Christina Quast <contact@christina-quast.de>

--- drivers/hid/hid-ft260.c
+++ drivers/hid/hid-ft260.c
@@ -220,8 +220,8 @@ struct ft260_i2c_read_request_report {
 	__le16 length;		/* data payload length */
 } __packed;
 
-struct ft260_i2c_input_report {
-	u8 report;		/* FT260_I2C_REPORT */
+struct ft260_input_report {
+	u8 report;		/* FT260_I2C_REPORT or FT260_UART_REPORT */
 	u8 length;		/* data payload length */
 	u8 data[2];		/* data payload */
 } __packed;
@@ -1066,7 +1066,7 @@ static int ft260_raw_event(struct hid_device *hdev, struct hid_report *report,
 			   u8 *data, int size)
 {
 	struct ft260_device *dev = hid_get_drvdata(hdev);
-	struct ft260_i2c_input_report *xfer = (void *)data;
+	struct ft260_input_report *xfer = (void *)data;
 
 	if (xfer->report >= FT260_I2C_REPORT_MIN &&
 	    xfer->report <= FT260_I2C_REPORT_MAX) {
